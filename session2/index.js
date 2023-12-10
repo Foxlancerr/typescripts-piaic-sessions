b43981b57aac11c539c8308dbf49b8f7f41fb61c
M-Asimm <kpkprince55persia@gmail.com>
2023-12-10 10:08:46 +0500

    session2 is done

---
+++ session2/index.js
@@ -0,0 +1,133 @@
+// console.log("hello world");
+
+// assigment operator
+
+
+
+
+// comparison operator
+
+
+
+
+
+// logical operator
+//and &&
+
+// true && true = true
+// true && false = false
+// false && false = false
+// false && true = false
+
+
+// OR||
+// true || true = true
+// false || true = true
+// true || false = true
+// false || false = false
+
+// console.log(true && true); //true
+// console.log(false && true); // false
+// console.log(false && false); //false
+
+// console.log(true || true); // true
+// console.log(true || false); //true
+// console.log(false || false); //false
+
+// console.log(true);
+// console.log(!true);
+
+// console.log("");
+// console.log(0);
+// console.log(null);
+// console.log(undefined);
+
+/**
+ * true && true && true && true   will return the last true value
+ * true && null && 0 && true   will stop at first falsy value and return the first falsy value
+ * true || true || true || true   will return the last true value
+ */
+
+console.log("object" && true && true && "54");
+console.log("" && false && false && "kamran" && true);
+
+
+/**
+ * true || true || false || true   will return the first true value
+ * true || true || true || false   will stop at first truety value and return the first falsy value
+ * false || true || true || false   will stop at first truety value and return the first falsy value
+ */
+
+console.log("object" || true || true || "54");
+console.log("" || false || false || "kamran" || true);
+
+
+
+
+// projects
+//BMI calculator
+
+
+
+// miles to kilometers conversion
+//1 miles = 1.6093km
+
+
+
+
+
+//chapter3 Array & Objects
+// arrays
+
+
+
+
+// creating arrays,modifying arrays,overiting arrays
+
+
+
+
+
+
+// arrays properties
+//push
+//pop
+//length
+//unshift
+//shift
+//splice(index,no of elements,new element)
+//sort
+//reverse
+//concat
+
+
+
+
+
+
+
+//multidementainals arrays
+
+
+
+
+
+
+
+
+//object in javascript
+
+
+
+
+//updating opjects
+
+
+
+// arrays in objects
+
+
+// object methods
+//object.keys
+//object.values
+//object.entries
